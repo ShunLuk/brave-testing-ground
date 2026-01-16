112c661c41c38f04e7157253cb43d8ec02fba9e7
Shun Luk <ngoshun.luk@gmail.com>
2026-01-16 16:04:21 +0100

    * Added script and styling to turn faqs into accordions  * Added filters for amount, subjects and ministries

--- web/app/themes/sage/app/View/Composers/VacComposer.php
+++ web/app/themes/sage/app/View/Composers/VacComposer.php
@@ -4,18 +4,32 @@ declare(strict_types=1);
 
 namespace App\View\Composers;
 
-use Roots\Acorn\View\Composer;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Roots\Acorn\View\Composer;
 
 class VacComposer extends Composer
 {
+	private $amount;
+	private $page;
+	private $subjects;
+	private $ministries;
+
+	public function __construct(Request $request)
+	{
+		$this->amount = $request->query('amount') ?? 25;
+		$this->page = $request->query('page');
+		$this->subjects = $request->query('subjects');
+		$this->ministries = $request->query('ministries');
+	}
+
 	/**
 	 * List of views served by this composer.
 	 *
 	 * @var string[]
 	 */
 	protected static $views = [
-		'template-vacs'
+		'template-vacs',
 	];
 
 	/**
@@ -26,6 +40,8 @@ class VacComposer extends Composer
 	{
 		return [
 			'faqs' => $this->getFaqs(),
+			'subjects' => $this->getSubjects(),
+			'ministries' => $this->getMinistries(),
 		];
 	}
 
@@ -40,10 +56,14 @@ class VacComposer extends Composer
 		return false;
 	}
 
-	private function makeApiCallOpendata($endpoint): array
+	private function makeApiCallOpendata($endpoint, $params = []): array
 	{
 		$baseUrl = 'https://opendata.rijksoverheid.nl/v1/infotypes/';
 		$url = $baseUrl . "{$endpoint}?output=json";
+
+		foreach ($params as $key => $value) {
+			$url .= "&{$key}={$value}";
+		}
 
 		$response = $this->makeApiCall($url);
 		if ($response) {
@@ -53,19 +73,123 @@ class VacComposer extends Composer
 		return [];
 	}
 
+	private function getAllSubjects(): array
+	{
+		$params = [];
+
+		return $this->makeApiCallOpendata('subject', $params);
+	}
+
+	private function getAllMinistries(): array
+	{
+		$params = [];
+
+		return $this->makeApiCallOpendata('ministry', $params);
+	}
+
 	private function getSingleFaq($faqId): array
 	{
 		return $this->makeApiCallOpendata("faq/{$faqId}");
 	}
 
+	private function getTheFaqs($rows = 25, $offset = null): array
+	{
+		$params = [
+			'rows' => $rows,
+		];
+		if ($offset) {
+			$params['offset'] = $offset;
+		}
+
+		return $this->makeApiCallOpendata('faq', $params);
+	}
+
+	private function getSubjectFaqs($subject, $rows = 25, $offset = null)
+	{
+		$params = [
+			'rows' => $rows,
+		];
+
+		if ($offset) {
+			$params['offset'] = $offset;
+		}
+
+		return $this->makeApiCallOpendata("faq/subjects/{$subject}", $params);
+	}
+
+	private function getMinistryFaqs($ministry, $rows = 25, $offset = null)
+	{
+		$params = [
+			'rows' => $rows,
+		];
+
+		if ($offset) {
+			$params['offset'] = $offset;
+		}
+
+		return $this->makeApiCallOpendata("faq/ministries/{$ministry}", $params);
+	}
+
 	private function getAllFaqs(): array
 	{
-		return $this->makeApiCallOpendata('faq');
+		$amount = $this->amount;
+		$faqs = $this->getTheFaqs($amount);
+
+		$nextFaqs = $this->getTheFaqs($amount + 1);
+
+		//		$page = $this->page;
+		//		if ($page) {
+		//
+		//		}
+
+		return $faqs;
 	}
+
+	private function getMinistries(): object
+	{
+		$ministries = [];
+		$ministriesObjects = $this->getAllMinistries();
+		foreach ($ministriesObjects as $ministryObject) {
+			$ministry = [
+				'id' => $ministryObject['id'],
+				'name' => $ministryObject['name'],
+				'label' => $ministryObject['title'],
+			];
+			$ministries[] = (object)$ministry;
+		}
+
+		return (object)$ministries;
+	}
+
+	private function getSubjects(): object
+	{
+		$subjects = [];
+		$subjectsObjects = $this->getAllSubjects();
+		foreach ($subjectsObjects as $subjectObject) {
+			$subject = [
+				'id' => $subjectObject['id'],
+				'name' => $subjectObject['name'],
+				'label' => $subjectObject['title'],
+			];
+			$subjects[] = (object)$subject;
+		}
+		sort($subjects);
+
+		return (object)$subjects;
+	}
+
 	public function getFaqs(): array
 	{
 		$faqs = [];
-		$allFaqs = $this->makeApiCallOpendata('faq') ?? [];
+
+		if (! is_null($this->subjects) && '' != $this->subjects) {
+			$allFaqs = $this->getSubjectFaqs($this->subjects) ?? [];
+		} elseif (! is_null($this->ministries) && '' != $this->ministries) {
+			$allFaqs = $this->getMinistryFaqs($this->ministries) ?? [];
+		} else {
+			$allFaqs = $this->getAllFaqs() ?? [];
+		}
+
 		foreach ($allFaqs as $faqObject) {
 			$faqId = $faqObject['id'];
 			$faqResponse = $this->getSingleFaq($faqId);
--- web/app/themes/sage/resources/views/template-vacs.blade.php
+++ web/app/themes/sage/resources/views/template-vacs.blade.php
@@ -7,38 +7,99 @@
 <x-layout>
 	<h1>Veelgestelde Vragen</h1>
 	<div class="faqs-wrapper">
-		@if(count($faqs))
-			@foreach($faqs as $faq)
-				<div class="faq-item faq-item-{{ $faq->id }}">
-					<h2 class="faq-item-question">{{ $faq->question }}</h2>
-					<div class="faq-item-answer">
-						@if($faq->introduction)
-							<div class="faq-item-answer-intro">
-								{!! $faq->introduction !!}
-							</div>
-						@endif
-						@if ($faq->content && count($faq->content))
-							@foreach($faq->content as $faqParagraph)
-								<div class="faq-item-answer-paragraph">
-									@if(array_key_exists('paragraphtitle', $faqParagraph))
-										<h3 class="faq-item-answer-paragraph-title">{{ $faqParagraph['paragraphtitle'] }}</h3>
-									@endif
-									@if (array_key_exists('paragraph', $faqParagraph))
-										{!! $faqParagraph['paragraph'] !!}
-									@endif
-								</div>
-							@endforeach
-						@endif
-					</div>
-					@if($faq->canonical)
-						<a href="{{ $faq->canonical }}" target="_blank" title="{{ $faq->question }}">
-							{{ $faq->question }}
-						</a>
-					@endif
+		<form class="faqs-filters">
+			@php($selectedAmount = app('request')->input('amount') ?? 0)
+			<div class="faq-filter">
+				<label for="amount">Hoeveelheid vragen tonen</label>
+				<select name="amount">
+					<option value="25"{{ ($selectedAmount == 25 ? ' selected' : '') }}>25</option>
+					<option value="50"{{ ($selectedAmount == 50 ? ' selected' : '') }}>50</option>
+					<option value="100"{{ ($selectedAmount == 100 ? ' selected' : '') }}>100</option>
+					<option value="150"{{ ($selectedAmount == 150 ? ' selected' : '') }}>150</option>
+					<option value="200"{{ ($selectedAmount == 200 ? ' selected' : '') }}>200</option>
+				</select>
+			</div>
+			@if ($subjects)
+				@php($selectedSubject = app('request')->input('subjects') ?? '')
+				<div class="faq-filter">
+					<label for="subjects">Filteren op onderwerpen</label>
+					<select name="subjects">
+						<option value="">Kies een onderwerp</option>
+						@foreach($subjects as $subject)
+							<option value="{{ $subject->name }}">{{ $subject->label }}</option>
+						@endforeach
+					</select>
 				</div>
-			@endforeach
+			@endif
+			@if ($ministries)
+				@php($selectedMinistry = app('request')->input('ministries') ?? '')
+				<div class="faq-filter">
+					<label for="ministries">Filteren op ministeries</label>
+					<select name="ministries">
+						<option value="">Kies een ministerie</option>
+						@foreach($ministries as $ministry)
+							<option value="{{ $ministry->name }}">{{ $ministry->label }}</option>
+						@endforeach
+					</select>
+				</div>
+			@endif
+			<button class="button" type="submit">Filter</button>
+		</form>
+		@if(count($faqs))
+			<div class="faqs ac-panel">
+				@foreach($faqs as $faq)
+					<div class="faq-item faq-item-{{ $faq->id }}">
+						<a class="faq-item-question" href="javascript:toggleFaq('{{ $faq->id }}')">
+							<h2>{{ $loop->iteration }}. {{ $faq->question }}</h2>
+							<span class="faq-item-toggler closed">+</span>
+							<span class="faq-item-toggler open">-</span>
+						</a>
+						<div class="faq-item-answer">
+							@if($faq->introduction)
+								<div class="faq-item-answer-intro">
+									{!! $faq->introduction !!}
+								</div>
+							@endif
+							@if (property_exists($faq, 'content') && count($faq->content))
+								@foreach($faq->content as $faqParagraph)
+									<div class="faq-item-answer-paragraph">
+										@if(array_key_exists('paragraphtitle', $faqParagraph))
+											<h3 class="faq-item-answer-paragraph-title">{{ $faqParagraph['paragraphtitle'] }}</h3>
+										@endif
+										@if (array_key_exists('paragraph', $faqParagraph))
+											{!! $faqParagraph['paragraph'] !!}
+										@endif
+									</div>
+								@endforeach
+							@endif
+							@if($faq->canonical)
+								<a href="{{ $faq->canonical }}" target="_blank" title="{{ $faq->question }}">
+									{{ $faq->question }}
+								</a>
+							@endif
+						</div>
+					</div>
+				@endforeach
+			</div>
+			<div class="faq-pagination">
+
+			</div>
 		@else
 			Er zijn geen veelgestelde vragen gevonden
 		@endif
 	</div>
 </x-layout>
+<script>
+	function toggleFaq(faqId) {
+		var faqSelector = `.faq-item-${faqId}`;
+		var faqWrapper = document.querySelector(faqSelector);
+
+		if (!faqWrapper.classList.contains('open') && !faqWrapper.classList.contains('closed')) {
+			faqWrapper.classList.add('open');
+		} else {
+			faqWrapper.classList.toggle('open');
+			faqWrapper.classList.toggle('closed');
+		}
+		console.log(faqWrapper.classList);
+	}
+</script>
