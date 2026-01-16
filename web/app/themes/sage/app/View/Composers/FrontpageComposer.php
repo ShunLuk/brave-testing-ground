59d9377678c30f437965215c2b5185b73e0646a3
Shun Luk <ngoshun.luk@gmail.com>
2026-01-16 12:05:53 +0100

    Make buttons optional

--- web/app/themes/sage/app/View/Composers/FrontpageComposer.php
+++ web/app/themes/sage/app/View/Composers/FrontpageComposer.php
@@ -1,60 +1,62 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\View\Composers;
 
 use Roots\Acorn\View\Composer;
 
 class FrontpageComposer extends Composer
 {
-    /**
-     * List of views served by this composer.
-     *
-     * @var string[]
-     */
-    protected static $views = [
-        'front-page',
-    ];
+	/**
+	 * List of views served by this composer.
+	 *
+	 * @var string[]
+	 */
+	protected static $views = [
+		'front-page',
+	];
 
-    /**
-     * This will make the variable `$roots` available in the 'example' partial
-     * with the value described here.
-     */
-    public function with()
-    {
-        return [
-            'banner' => $this->getBannerData(),
-        ];
-    }
+	/**
+	 * This will make the variable `$roots` available in the 'example' partial
+	 * with the value described here.
+	 */
+	public function with()
+	{
+		return [
+			'banner' => $this->getBannerData(),
+		];
+	}
 
-    private function getBannerData()
-    {
-        $title = get_field('title') ?? 'Hello World!';
-        $subtitle = get_field('subtitle') ?? 'Just another Brave Theme';
-        $banner_id = get_field('banner_image') ?? '';
-        $buttons = get_field('buttons') ?? [];
+	private function getBannerData()
+	{
+		$title = get_field('title') ?? 'Hello World!';
+		$subtitle = get_field('subtitle') ?? 'Just another Brave Theme';
+		$banner_id = get_field('banner_image') ?? '';
+		$buttons = get_field('buttons') ?? [];
 
-        if ($banner_id && $banner_id != '') {
-            $banner = wp_get_attachment_image($banner_id, 'large', false, ['class'=> '']);
-        }
+		if ($banner_id && '' != $banner_id) {
+			$banner = wp_get_attachment_image($banner_id, 'large', false, ['class' => '']);
+		}
 
-        $banner = [
-            'title' => $title,
-            'subtitle' => $subtitle,
-            'image' => $banner,
-        ];
+		$banner = [
+			'title' => $title,
+			'subtitle' => $subtitle,
+			'image' => $banner,
+		];
 
-        if ($buttons && count($buttons)) {
-            $buttonsArr = [];
-            foreach($buttons as $buttonKey => $button) {
-				if ($button && $button !== '') {
+		if ($buttons && count($buttons)) {
+			$buttonsArr = [];
+			foreach ($buttons as $buttonKey => $button) {
+				if ($button && '' !== $button) {
 					$buttonsArr[$buttonKey] = (object)$button;
 				}
-            }
-			if(count($buttonsArr)) {
+			}
+			if (count($buttonsArr)) {
 				$banner['buttons'] = (object)$buttonsArr;
 			}
-        }
+		}
 
-        return (object) $banner;
-    }
+		return (object) $banner;
+	}
 }
